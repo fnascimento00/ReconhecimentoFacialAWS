48ea8143d95d3b4488e0ca5a9c2d273e03165411
flavio.nascimento <flavio.nascimento@pfmconsultoria.com.br>
2023-07-04 15:49:47 -0300

    Adicionada a api javascript face-api para fazer o reconhecimento do rosto em tempo de execução. Melhorias na rotina de aproximação do rosto.

--- Site/Controllers/LoginController.cs
+++ Site/Controllers/LoginController.cs
@@ -2,7 +2,9 @@
 using Microsoft.AspNetCore.Mvc;
 using ReconhecimentoFacialAWS.Domains.Receivers;
 using ReconhecimentoFacialAWS.Mappers;
+using ReconhecimentoFacialAWS.Repositories;
 using ReconhecimentoFacialAWS.ViewModels;
+using System.ComponentModel.DataAnnotations;
 using System.Security.Claims;
 
 namespace ReconhecimentoFacialAWS.Controllers;
@@ -11,16 +13,34 @@ public class LoginController : Controller
 {
     private readonly ILoginUserREC _LoginUser;
     private readonly IAddUserREC _addUser;
+    private readonly IUserRepository _userRepository;
 
     public LoginController(ILoginUserREC loginUser,
-                           IAddUserREC addUser)
+                           IAddUserREC addUser,
+                           IUserRepository userRepository)
     {
         _LoginUser = loginUser;
         _addUser = addUser;
+        _userRepository = userRepository;
     }
 
     public IActionResult Index()
     {
+        if (HttpContext.User.Identity.IsAuthenticated)
+        {
+            var _claimUser = HttpContext.User.Claims.FirstOrDefault(x => x.Type == "User");
+            var _photoUser = _userRepository.GetPhoto(_claimUser.Value);
+
+            if (string.IsNullOrWhiteSpace(_photoUser))
+            {
+                return RedirectToAction("Index", "CapturePhoto");
+            }
+            else
+            {
+                return RedirectToAction("Index", "Recognition");
+            }
+        }
+
         return View();
     }
 
@@ -32,16 +52,7 @@ public class LoginController : Controller
             var _command = Mapper.MapToCommand(vm);
             var _validate = _LoginUser.Validate(_command);
 
-            if (string.IsNullOrWhiteSpace(_validate))
-            {
-                await Authenticate(vm);
-
-                return Json(new
-                {
-                    valid = true
-                });
-            }
-            else
+            if (!string.IsNullOrWhiteSpace(_validate))
             {
                 return Json(new
                 {
@@ -49,6 +60,15 @@ public class LoginController : Controller
                     message = _validate
                 });
             }
+
+            await Authenticate(vm);
+            var _user = _userRepository.GetUser(vm.Login);
+
+            return Json(new
+            {
+                valid = true,
+                redirectTo = _user.Photo == null ? "CapturePhoto" : "Recognition"
+            });
         }
 
         return Json(new
@@ -82,17 +102,7 @@ public class LoginController : Controller
             var _command = Mapper.MapToCommand(vm);
             var _validate = _addUser.Validate(_command);
 
-            if (string.IsNullOrWhiteSpace(_validate)) 
-            { 
-                var _execute = _addUser.Execute(_command);
-
-                return Json(new
-                {
-                    valid = true,
-                    message = _execute
-                });
-            }
-            else
+            if (!string.IsNullOrWhiteSpace(_validate)) 
             {
                 return Json(new
                 {
@@ -100,12 +110,39 @@ public class LoginController : Controller
                     message = _validate
                 });
             }
+
+            var _execute = _addUser.Execute(_command);
+
+            return Json(new
+            {
+                valid = true,
+                message = _execute
+            });
         }
 
         return Json(new
         {
             valid = false,
             message = "Dados Inválidos!"
+        });
+    }
+
+    [HttpPost]
+    public async Task<JsonResult> Disconnect()
+    {
+        if (!HttpContext.User.Identity.IsAuthenticated)
+        {
+            return Json(new
+            {
+                valid = true
+            });
+        }
+
+        await HttpContext.SignOutAsync();
+
+        return Json(new
+        {
+            valid = true
         });
     }
 }
---
+++ Site/Domains/Commands/AddPhotoUserCOM.cs
@@ -0,0 +1,7 @@
+﻿namespace ReconhecimentoFacialAWS.Domains.Commands;
+
+public class AddPhotoUserCOM
+{
+    public string Login { get; set; }
+    public string Photo { get; set; }
+}
